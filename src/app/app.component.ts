a1c1bce73fa266d45443a910ab3b5aa876acb32e
Kairuus <kingkairuus@gmail.com>
2023-07-25 17:39:58 -0300

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface KeyHistory {
+  pressedKey: string;
+  charCode: number;
+  altKey: boolean;
+  metaKey: boolean;
+  controlKey: boolean;
+  shiftKey: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'key-value';
+
+  keysHistory: KeyHistory[] = [];
+
+  onKeyDown(event: KeyboardEvent): void {
+
+    const pressedKey: string = event.key;
+    const charCode: number = event.key.charCodeAt(0);
+    const altKey: boolean = event.altKey;
+    const metaKey: boolean = event.metaKey;
+    const controlKey: boolean = event.ctrlKey;
+    const shiftKey: boolean = event.shiftKey;
+
+    this.keysHistory.unshift({
+      pressedKey,
+      charCode,
+      altKey,
+      metaKey,
+      controlKey,
+      shiftKey
+    });
+
+  }
 }
